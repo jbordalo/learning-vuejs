89f2d19c23e4da95d565b48dc239202fec860de0
jbordalo <joao-bordalo@hotmail.com>
2021-04-08 15:08:01 +0100

    Add quotes program

---
+++ vue-quotes/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container">
+    <app-header :maxQuotes="maxQuotes" :quoteCount="quotes.length"></app-header>
+    <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+    <app-quote-grid
+      :quotes="quotes"
+      @quoteDeleted="deleteQuote"
+    ></app-quote-grid>
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">Info: Click on a Quote to delete it.</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import NewQuote from "./components/NewQuote.vue";
+import QuoteGrid from "./components/QuoteGrid.vue";
+import Header from "./components/Header.vue";
+
+export default {
+  data() {
+    return {
+      quotes: ["Just a quote!"],
+      maxQuotes: 10,
+    };
+  },
+  methods: {
+    newQuote(quote) {
+      if (this.quotes.length >= this.maxQuotes) {
+        return alert("Please delete some quotes first!");
+      }
+      this.quotes.push(quote);
+    },
+    deleteQuote(index) {
+      this.quotes.splice(index, 1);
+    },
+  },
+  components: {
+    appQuoteGrid: QuoteGrid,
+    appNewQuote: NewQuote,
+    appHeader: Header,
+  },
+};
+</script>
+
+<style></style>
---
+++ vue-quotes/src/components/NewQuote.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="row">
+    <div
+      class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group"
+    >
+      <label>Quote</label>
+      <textarea
+        class="form-control"
+        rows="3"
+        v-model="quote"
+        placeholder="Your quote"
+        ref="quoteInput"
+      ></textarea>
+    </div>
+    <div
+      class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group"
+    >
+      <button class="btn btn-rimary" @click.prevent="createNew">
+        Add Quote
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      quote: "",
+    };
+  },
+  methods: {
+    createNew() {
+      if (this.quote === "") {
+        this.$refs.quoteInput.focus();
+        return alert("Please type a quote!");
+      }
+      this.$emit("quoteAdded", this.quote);
+      this.quote = "";
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
